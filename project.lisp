452f7a6e11dc3496567f94b0a378d5252358cecf
JoaoBraveCoding <joao.marcal12@hotmail.com>
2015-10-26 15:49:48 +0000

    Done until tabuleiros-iguais-p

--- project.lisp
+++ project.lisp
@@ -27,12 +27,59 @@
     (return-from copia-tabuleiro tabuleiroNovo)))
 
 ;;tabuleiro-preenchido-p: tabuleiro x inteiro -> logico
-(defun tabuleiro-preenchido-p (tabuleiro inteiro inteiro)
-  (aref tabuleiro inteiro inteiro inteiro))
+(defun tabuleiro-preenchido-p (tabuleiro inteiroLinha inteiroColuna)
+  (aref tabuleiro inteiroLinha inteiroColuna))
 
 ;;tabuleiro-altura-coluna: tabuleiro x inteiro -> inteiro
 (defun tabuleiro-altura-coluna (tabuleiro inteiro)
-  (loop for i from 17 to 0
-        do(if ((aref tabuleiro i inteiro) t)
-              (return-from tabuleiro-altura-coluna i)))
-        (return-from tabuleiro-altura-coluna nil))
+  (loop for i from 0 to 17
+        do(cond ((eq (aref tabuleiro (- 17 i) inteiro) t) (return-from tabuleiro-altura-coluna (- 18 i)))
+          )
+        )
+  (return-from tabuleiro-altura-coluna 0)
+  )
+
+;;tabuleiro-linha-completa-p: tabuleiro x inteiro -> logico
+(defun tabuleiro-linha-completa-p (tabuleiro inteiro)
+  (loop for i from 0 to 9
+        do(cond ((eq (aref tabuleiro inteiro i) nil) (return-from tabuleiro-linha-completa-p nil))
+                )
+        )
+  (return-from tabuleiro-linha-completa-p t)
+  )
+
+;;tabuleiro-preenche!: tabuleiro inteiro inteiro -> {}
+(defun tabuleiro-preenche! (tabuleiro inteiroLinha inteiroColuna)
+  (setf (aref tabuleiro inteiroLinha inteiroColuna) t)
+  )
+
+;;tabuleiro-remove-linha!: tabuleiro inteiro -> {}
+(defun tabuleiro-remove-linha! (tabuleiro inteiro)
+  (loop for i from inteiro to 16
+        do(loop for j from 0 to 9
+                do(setf (aref tabuleiro i j) (aref tabuleiro (+ 1 i) j)))
+   )
+  (loop for i from 0 to 9
+        do(setf (aref tabuleiro 17 i) nil)
+   )
+  )
+
+;;tabuleiro-topo-preenchido-p: tabuleiro -> logico
+(defun tabuleiro-topo-preenchido-p (tabuleiro)
+  (loop for i from 0 to 9
+        do(cond ((eq (aref tabuleiro 17 i) t) (return-from tabuleiro-topo-preenchido-p t))
+                )
+        )
+  (return-from tabuleiro-topo-preenchido-p nil)
+  )
+
+;;tabuleiros-iguais-p: tabuleiro x tabuleiro ->logico
+(defun tabuleiros-iguais-p (tabuleiro1 tabuleiro2)
+  (loop for i from 0 to 9
+        do(loop for j from 0 to 17
+                do(cond((not(eq (aref tabuleiro1 j i) (aref tabuleiro2 j i))) (return-from tabuleiros-iguais-p nil))
+                  )
+        )
+  )
+  (return-from tabuleiros-iguais-p t)
+)
